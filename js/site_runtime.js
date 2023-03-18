91a6227e5bd18ad0072ae2d27eea1a1722d7f77f
cicyic <zhiyupink@gmail.com>
2023-03-18 10:06:40 +0800

    css+js

---
+++ js/site_runtime.js
@@ -0,0 +1,42 @@
+setInterval(() => {
+    // let create_time = Math.round(new Date('2021-10-15 00:00:00').getTime() / 1000); //在此行修改建站时间
+    // 有苹果用户发现safari浏览器不能正常运行，百度了一下发现是格式化的问题，改成下面这种应该就可以了。感谢反馈。
+    let create_time = Math.round(new Date('2021/10/31 23:59:59').getTime() / 1000); //在此行修改建站时间
+    let timestamp = Math.round((new Date().getTime()) / 1000);
+    let second = timestamp - create_time;
+    let time = new Array(0, 0, 0, 0, 0);
+
+    var nol = function(h) {
+        return h > 9 ? h : '0' + h;
+    }
+    // year + days + time
+    if (second >= 365 * 24 * 3600) {
+        time[0] = parseInt(second / (365 * 24 * 3600));
+        second %= 365 * 24 * 3600;
+    }
+    if (second >= 24 * 3600) {
+        // days + time
+        time[1] = parseInt(second / (24 * 3600)) + time[0]*365;
+        console.log(time[1])
+        second %= 24 * 3600;
+    }
+    if (second >= 3600) {
+        time[2] = nol(parseInt(second / 3600));
+        second %= 3600;
+    }
+    if (second >= 60) {
+        time[3] = nol(parseInt(second / 60));
+        second %= 60;
+    }
+    if (second >= 0) {
+        time[4] = nol(second);
+    }
+    let currentTimeHtml = ""
+    if (time[0] != 0) {
+        currentTimeHtml += time[0] + '年 '
+    }
+    // days + time: delete year show
+    currentTimeHtml = []
+    currentTimeHtml += '小破站成功运行 ' + time[1] + ' 天 ' + time[2] + ' 小时 ' + time[3] + ' 分 ' + time[4] + ' 秒';
+    document.getElementById("runtime").innerHTML = currentTimeHtml;
+}, 1000);
